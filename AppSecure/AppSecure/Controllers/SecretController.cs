06ae0a3481a8d265a33a62ccb56d6b5b9d7b483b
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2018-11-15 16:08:27 -0600

    Agregamos la autenticcion a los controladores

---
+++ AppSecure/AppSecure/Controllers/SecretController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppSecure.Controllers
+{
+    public class SecretController : Controller
+    {
+        // GET: Secret
+        [Authorize]
+        //[Authorize (Roles="Admin, erik.guerrero@uttt.edu.mx")]
+        public ContentResult Secret()
+        {
+            return Content("Este es un Secreto");
+        }
+        [AllowAnonymous]
+        public ContentResult Overt()
+        {
+            return Content("Este no es un Secreto");
+        }
+    }
+}
